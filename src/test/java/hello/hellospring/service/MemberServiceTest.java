08f51d8bdd09782454eb36a708946181aee4519d
ttochi3 <ttochi3795@gmail.com>
2023-09-21 22:27:15 +0900

    first dependency injection

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,19 +1,75 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.AbstractStringAssert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    //MemberService memberService = new MemberService();
+    //MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach // 테스트를 하나씩 끝내면,
+    public void afterEach() {
+        memberRepository.clearStore(); // 메모리를 초기화한다.
+    }
 
     @Test
     void 회원가입() {
         //given
+        Member member = new Member();
+        member.setName("spring");
 
         //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+/*
+        try {
+            memberService.join(member2);
+            fail();
+        } catch(IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.123");
+        }
+*/
+        // assertThrows(IllegalStateException.class, () -> memberService.join(member2)); //예외가 터져야 성공.
+
+        //또는 assertThrows(IllegalStateException.class, () -> memberService.join(member2)); 까지 치고 Ctrl + Alt + V 하면
+        //IllegalStateException illegalStateException = assertThrows(IllegalStateException.class, () -> memberService.join(member2)); 라고 변환되고
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
         //then
     }
