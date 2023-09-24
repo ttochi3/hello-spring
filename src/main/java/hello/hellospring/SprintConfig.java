5fb1323847ca69328600ffd7ba5b2d1e73e87ad3
ttochi3 <ttochi3795@gmail.com>
2023-09-24 15:32:03 +0900

    스프링 DB 접근 기술 - 순수 JDBC

--- src/main/java/hello/hellospring/SprintConfig.java
+++ src/main/java/hello/hellospring/SprintConfig.java
@@ -1,14 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 자바코드로 직접 스프링 빈 등록하기
 @Configuration
 public class SprintConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SprintConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     // MemberService 에서 @Service 와 @Autowired 를 사용하지 않는 경우 직접 스프링빈으로 등록한다.
     @Bean
@@ -19,6 +29,7 @@ public class SprintConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+         // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
